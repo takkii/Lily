cada179b47ea3bf7a8b269e18bca1d7e33a35d4d
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-07-01 12:31:21 +0900

    Update, syntax check and % → / change.

--- python/Hello.py
+++ python/Hello.py
@@ -1,11 +1,13 @@
 def hello():
-	doc = XSCRIPTCONTEXT.getDocument()
-	sheet = doc.getSheets().getByName('Python_macro1')
-	A1 = sheet.getCellRangeByName('A1')
-	B1 = sheet.getCellRangeByName('B1')
-	A2 = sheet.getCellRangeByName('A2')
-	B2 = sheet.getCellRangeByName('B2')
-	A1.String = 'Hello'
-	B1.String = 'World'
-	A2.String = A1.String + B1.String
-	B2.String = A1.String + B1.String + '1234'
+  doc = XSCRIPTCONTEXT.getDocument()
+  sheet = doc.getSheets().getByName('Python_macro1')
+  A1 = sheet.getCellRangeByName('A1')
+  B1 = sheet.getCellRangeByName('B1')
+  A2 = sheet.getCellRangeByName('A2')
+  B2 = sheet.getCellRangeByName('B2')
+
+  A1.String = 'Hello'
+  B1.String = 'World'
+
+  A2.String = A1.String + B1.String
+  B2.String = A1.String + B1.String + '1234'
